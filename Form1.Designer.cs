0a2c56629b61f201c735a7af1fd8da1050e1f93a
LAmorim <leandrox.amorim@gmail.com>
2024-12-28 17:57:46 -0300

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace JoKenPo
+{
+    partial class JoKenPo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblYou = new System.Windows.Forms.Label();
+            this.lblPc = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblVs = new System.Windows.Forms.Label();
+            this.lblEqual = new System.Windows.Forms.Label();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.picResultado = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picResultado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 33F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(776, 67);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Escolha: Pedra, Papel ou Tesoura...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblYou
+            // 
+            this.lblYou.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYou.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblYou.Location = new System.Drawing.Point(75, 79);
+            this.lblYou.Name = "lblYou";
+            this.lblYou.Size = new System.Drawing.Size(81, 23);
+            this.lblYou.TabIndex = 4;
+            this.lblYou.Text = "Você";
+            this.lblYou.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblPc
+            // 
+            this.lblPc.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPc.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblPc.Location = new System.Drawing.Point(356, 79);
+            this.lblPc.Name = "lblPc";
+            this.lblPc.Size = new System.Drawing.Size(81, 23);
+            this.lblPc.TabIndex = 5;
+            this.lblPc.Text = "PC";
+            this.lblPc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblResult
+            // 
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblResult.Location = new System.Drawing.Point(639, 164);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(81, 23);
+            this.lblResult.TabIndex = 6;
+            this.lblResult.Text = "?";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblVs
+            // 
+            this.lblVs.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVs.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblVs.Location = new System.Drawing.Point(237, 164);
+            this.lblVs.Name = "lblVs";
+            this.lblVs.Size = new System.Drawing.Size(49, 23);
+            this.lblVs.TabIndex = 7;
+            this.lblVs.Text = "vs";
+            this.lblVs.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblEqual
+            // 
+            this.lblEqual.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEqual.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblEqual.Location = new System.Drawing.Point(518, 164);
+            this.lblEqual.Name = "lblEqual";
+            this.lblEqual.Size = new System.Drawing.Size(43, 23);
+            this.lblEqual.TabIndex = 8;
+            this.lblEqual.Text = "=";
+            this.lblEqual.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.BackgroundImage = global::JoKenPo.Properties.Resources.Tesoura;
+            this.btnScissors.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnScissors.Location = new System.Drawing.Point(576, 296);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(200, 200);
+            this.btnScissors.TabIndex = 11;
+            this.btnScissors.UseVisualStyleBackColor = true;
+            this.btnScissors.Click += new System.EventHandler(this.btnScissors_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.BackgroundImage = global::JoKenPo.Properties.Resources.Papel;
+            this.btnPaper.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPaper.Location = new System.Drawing.Point(301, 296);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(200, 200);
+            this.btnPaper.TabIndex = 10;
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnRock
+            // 
+            this.btnRock.BackgroundImage = global::JoKenPo.Properties.Resources.Pedra;
+            this.btnRock.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRock.Location = new System.Drawing.Point(21, 296);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(200, 200);
+            this.btnRock.TabIndex = 9;
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // picResultado
+            // 
+            this.picResultado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picResultado.Location = new System.Drawing.Point(576, 79);
+            this.picResultado.Name = "picResultado";
+            this.picResultado.Size = new System.Drawing.Size(200, 200);
+            this.picResultado.TabIndex = 3;
+            this.picResultado.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(301, 79);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 175);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 79);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 175);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // JoKenPo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 513);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Controls.Add(this.lblEqual);
+            this.Controls.Add(this.lblVs);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblPc);
+            this.Controls.Add(this.lblYou);
+            this.Controls.Add(this.picResultado);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "JoKenPo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JoKenPo";
+            ((System.ComponentModel.ISupportInitialize)(this.picResultado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox picResultado;
+        private System.Windows.Forms.Label lblYou;
+        private System.Windows.Forms.Label lblPc;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblVs;
+        private System.Windows.Forms.Label lblEqual;
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissors;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JoKenPo
+{
+    public partial class JoKenPo : Form
+    {
+        public JoKenPo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            StartGame(0);
+        }
+
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            StartGame(2);
+        }
+
+        private void btnScissors_Click(object sender, EventArgs e)
+        {
+            StartGame(1);
+        }
+
+        private void StartGame(int opcao)
+        {
+            lblResult.Visible = false;
+            Game jogo = new Game();
+
+            switch (jogo.Jogar(opcao)) 
+            {
+                case Game.Resultado.Ganhar:
+                    picResultado.BackgroundImage = Image.FromFile("imagens/Ganhar.png");
+                    goto default;
+                case Game.Resultado.Perder:
+                    picResultado.BackgroundImage = Image.FromFile("imagens/Perder.png");
+                    goto default;
+                case Game.Resultado.Empatar:
+                    picResultado.BackgroundImage = Image.FromFile("imagens/Empatar.png");
+                    goto default;
+                default:
+                    pictureBox1.Image = jogo.ImgPlayer;
+                    pictureBox2.Image = jogo.ImgPC;
+                    break;
+
+            }
+
+        }
+    }
+}
